12a0d7bd2b55a59108aa5ac451d2ff4e4ff4baf9
voidstiles117 <65227494+voidstiles117@users.noreply.github.com>
2022-05-10 02:50:57 -0500

    Actualizado

--- src/calculos.java
+++ src/calculos.java
@@ -13,6 +13,23 @@ public class calculos{
                 double area = pi * Math.pow(radio, 2);
                 JOptionPane.showMessageDialog(null, "Tu area es: " + area);
             break;
+            case(2):
+                float ancho = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingresa el ancho de el rectangulo: "));
+                float alto = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingresa el alto de el rectangulo:"));
+                float resultado = ancho * alto;
+                JOptionPane.showMessageDialog(null, "El resultado es: " + resultado);
+            break;
+            case(3):
+                float altura = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese la altura de el triangulo: "));
+                float base = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese la base de el triangulo: "));
+                float areaTriangulo = base * altura / 2;
+                JOptionPane.showMessageDialog(null, "El area de el angulo es: " + areaTriangulo + "cm");
+            break;
+            case(4):
+                JOptionPane.showMessageDialog(null, "Saliste del programa");
+            break;
+            default:
+                JOptionPane.showMessageDialog(null, "La opcion que ingresaste no es valida");
             }
         }while (true);
     }
